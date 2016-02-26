94515da47285dfcd49287032329c27da1a5e7c47
t123yh <t123yh@outlook.com>
2016-02-26 22:58:32 +0800

    完成模型。

---
+++ ModelUNRegister/Models/EnrollModels.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace ModelUNRegister.Models
+{
+    public class EnrollRequest
+    {
+        [Key, Required]
+        public Guid RequestId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string School { get; set; }
+
+        [Required]
+        public DateTime RegisterTime { get; set; }
+
+        [Required]
+        public string SelfIntroduction { get; set; }
+
+        [Required]
+        public byte[] IPAddressData { get; set; }
+
+        private IPAddress _IPAddress;
+        [NotMapped]
+        public IPAddress IPAddress
+        {
+            get
+            {
+                if (_IPAddress == null)
+                    _IPAddress = new IPAddress(IPAddressData);
+                return _IPAddress;
+            }
+            set
+            {
+                IPAddressData = value.GetAddressBytes();
+                _IPAddress = value;
+            }
+        }
+
+    }
+}
---
+++ ModelUNRegister/Models/EnrollViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelUNRegister.Models
+{
+    public class EnrollViewModel
+    {
+        [Required]
+        [Display(Name = "姓名")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "所在学校")]
+        public string School { get; set; }
+
+        [Required]
+        [Display(Name = "自我介绍")]
+        public string SelfIntroduction { get; set; }
+    }
+}
