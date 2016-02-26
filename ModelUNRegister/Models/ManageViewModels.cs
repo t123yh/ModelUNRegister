758be85e676d7bbbd5cac38d4a5f4785ee4cc42e
t123yh <t123yh@outlook.com>
2016-02-26 22:51:29 +0800

    移除多余页面。

--- ModelUNRegister/Models/ManageViewModels.cs
+++ ModelUNRegister/Models/ManageViewModels.cs
@@ -14,30 +14,30 @@ namespace ModelUNRegister.Models
         public bool BrowserRemembered { get; set; }
     }
 
-    public class ManageLoginsViewModel
-    {
-        public IList<UserLoginInfo> CurrentLogins { get; set; }
-        public IList<AuthenticationDescription> OtherLogins { get; set; }
-    }
+    //public class ManageLoginsViewModel
+    //{
+    //    public IList<UserLoginInfo> CurrentLogins { get; set; }
+    //    public IList<AuthenticationDescription> OtherLogins { get; set; }
+    //}
 
-    public class FactorViewModel
-    {
-        public string Purpose { get; set; }
-    }
+    //public class FactorViewModel
+    //{
+    //    public string Purpose { get; set; }
+    //}
 
-    public class SetPasswordViewModel
-    {
-        [Required]
-        [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "新密码")]
-        public string NewPassword { get; set; }
+    //public class SetPasswordViewModel
+    //{
+    //    [Required]
+    //    [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 6)]
+    //    [DataType(DataType.Password)]
+    //    [Display(Name = "新密码")]
+    //    public string NewPassword { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "确认新密码")]
-        [Compare("NewPassword", ErrorMessage = "新密码和确认密码不匹配。")]
-        public string ConfirmPassword { get; set; }
-    }
+    //    [DataType(DataType.Password)]
+    //    [Display(Name = "确认新密码")]
+    //    [Compare("NewPassword", ErrorMessage = "新密码和确认密码不匹配。")]
+    //    public string ConfirmPassword { get; set; }
+    //}
 
     public class ChangePasswordViewModel
     {
@@ -58,29 +58,29 @@ namespace ModelUNRegister.Models
         public string ConfirmPassword { get; set; }
     }
 
-    public class AddPhoneNumberViewModel
-    {
-        [Required]
-        [Phone]
-        [Display(Name = "电话号码")]
-        public string Number { get; set; }
-    }
+    //public class AddPhoneNumberViewModel
+    //{
+    //    [Required]
+    //    [Phone]
+    //    [Display(Name = "电话号码")]
+    //    public string Number { get; set; }
+    //}
 
-    public class VerifyPhoneNumberViewModel
-    {
-        [Required]
-        [Display(Name = "代码")]
-        public string Code { get; set; }
+    //public class VerifyPhoneNumberViewModel
+    //{
+    //    [Required]
+    //    [Display(Name = "代码")]
+    //    public string Code { get; set; }
 
-        [Required]
-        [Phone]
-        [Display(Name = "电话号码")]
-        public string PhoneNumber { get; set; }
-    }
+    //    [Required]
+    //    [Phone]
+    //    [Display(Name = "电话号码")]
+    //    public string PhoneNumber { get; set; }
+    //}
 
-    public class ConfigureTwoFactorViewModel
-    {
-        public string SelectedProvider { get; set; }
-        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
-    }
+    //public class ConfigureTwoFactorViewModel
+    //{
+    //    public string SelectedProvider { get; set; }
+    //    public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+    //}
 }
