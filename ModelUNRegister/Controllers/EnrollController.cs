d948663f4f2481bc362875c119a9cd97ddb34cb4
t123yh <t123yh@outlook.com>
2016-04-17 01:02:49 +0800

    添加 Email 发送。

--- ModelUNRegister/Controllers/EnrollController.cs
+++ ModelUNRegister/Controllers/EnrollController.cs
@@ -7,6 +7,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ModelUNRegister.Models;
+using ModelUNRegister.Utilities;
+using System.Threading.Tasks;
 
 namespace ModelUNRegister.Controllers
 {
@@ -24,15 +26,30 @@ namespace ModelUNRegister.Controllers
         // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Name,School,Gender,Grade,Email,PhoneNumber,QQNumber")] EnrollRequest enrollRequest)
+        public async Task<ActionResult> Create([Bind(Include = "Name,School,Gender,Grade,Email,PhoneNumber,QQNumber")] EnrollRequest enrollRequest)
         {
             if (ModelState.IsValid)
             {
                 enrollRequest.RequestId = Guid.NewGuid();
                 enrollRequest.SubmissionTime = DateTime.Now;
                 enrollRequest.IPAddress = Request.UserHostAddress;
+                enrollRequest.EmailVerified = false;
                 db.EnrollRequests.Add(enrollRequest);
                 db.SaveChanges();
+                EmailVerificationViewModel emailModel = new EmailVerificationViewModel()
+                {
+                    Name = enrollRequest.Name,
+                    Id = enrollRequest.RequestId
+                };
+                await MailHelper.SendAsync(MailHelper.RenderPartialToString(this, "EmailVerification", emailModel),
+                   "注册",
+                   enrollRequest.Email,
+                   AppSettings.SMTPServer,
+                   AppSettings.SMTPPort,
+                   AppSettings.SMTPSSL,
+                   AppSettings.MailAccount,
+                   AppSettings.MailPassword);
+
                 return RedirectToAction("Success");
             }
 
