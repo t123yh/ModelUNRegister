f4ad083796323b7d19b96649f0f08d66bd3d724a
t123yh <t123yh@outlook.com>
2016-04-17 01:00:19 +0800

    添加对部分视图渲染的支持。

--- ModelUNRegister/Utilities/MailHelper.cs
+++ ModelUNRegister/Utilities/MailHelper.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ModelUNRegister.Utilities
 {
     public class MailHelper
     {
+        /// <summary>
+        /// 将指定的视图渲染至字符串内。
+        /// </summary>
+        /// <param name="controller">要渲染视图上下文所在的控制器。</param>
+        /// <param name="viewName">视图名称。</param>
+        /// <param name="model">要使用的模型。</param>
+        /// <returns></returns>
+        public static string RenderPartialToString(Controller controller, string viewName, object model)
+        {
+            controller.ViewData.Model = model;
+
+            using (StringWriter sw = new StringWriter())
+            {
+                ViewEngineResult viewResult = ViewEngines.Engines.FindPartialView(controller.ControllerContext, viewName);
+                ViewContext viewContext = new ViewContext(controller.ControllerContext, viewResult.View, controller.ViewData, controller.TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+
+
         /// <summary>
         /// 异步发送电子邮件。
         /// </summary>
@@ -23,6 +47,7 @@ namespace ModelUNRegister.Utilities
         /// <param name="password">发件者密码。</param>
         public static async Task SendAsync(string body,
             string subject,
+            string sourceDisplayName,
             string recipient,
             string smtpServer,
             int smtpPort,
@@ -31,7 +56,7 @@ namespace ModelUNRegister.Utilities
             string password)
         {
             var message = new MailMessage();
-            message.To.Add(new MailAddress(recipient));
+            message.To.Add(new MailAddress(recipient, sourceDisplayName));
             message.From = new MailAddress(sourceAddress);
             message.Subject = subject;
             message.Body = body;
