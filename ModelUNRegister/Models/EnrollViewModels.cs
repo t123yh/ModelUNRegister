3e8bf3bf6b790f475e1efbf703b06874f2978701
t123yh <t123yh@outlook.com>
2016-04-19 22:27:39 +0800

    移除不需要的视图模型。

--- ModelUNRegister/Models/EnrollViewModels.cs
+++ ModelUNRegister/Models/EnrollViewModels.cs
@@ -6,18 +6,5 @@ using System.Web;
 
 namespace ModelUNRegister.Models
 {
-    public class EnrollViewModel
-    {
-        [Required]
-        [Display(Name = "姓名")]
-        public string Name { get; set; }
-
-        [Required]
-        [Display(Name = "所在学校")]
-        public string School { get; set; }
-
-        [Required]
-        [Display(Name = "自我介绍")]
-        public string SelfIntroduction { get; set; }
-    }
+    
 }
