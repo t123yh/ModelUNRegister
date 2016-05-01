932d5c126e55732b300bb33ea66f744c31b6f6e6
t123yh <t123yh@outlook.com>
2016-05-01 22:21:06 +0800

    添加报名问题增删改。

---
+++ ModelUNRegister/Controllers/EnrollQuestionsController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ModelUNRegister.Models;
+
+namespace ModelUNRegister.Controllers
+{
+    [Authorize(Roles ="Administrators")]
+    public class EnrollQuestionsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: EnrollQuestions
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Questions.ToListAsync());
+        }
+
+        // GET: EnrollQuestions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EnrollQuestions/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "QuestionId,Question")] EnrollQuestion enrollQuestion)
+        {
+            if (ModelState.IsValid)
+            {
+                enrollQuestion.QuestionId = Guid.NewGuid();
+                db.Questions.Add(enrollQuestion);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(enrollQuestion);
+        }
+
+        // GET: EnrollQuestions/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EnrollQuestion enrollQuestion = await db.Questions.FindAsync(id);
+            if (enrollQuestion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollQuestion);
+        }
+
+        // POST: EnrollQuestions/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "QuestionId,Question")] EnrollQuestion enrollQuestion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enrollQuestion).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(enrollQuestion);
+        }
+
+        // GET: EnrollQuestions/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EnrollQuestion enrollQuestion = await db.Questions.FindAsync(id);
+            if (enrollQuestion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollQuestion);
+        }
+
+        // POST: EnrollQuestions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            EnrollQuestion enrollQuestion = await db.Questions.FindAsync(id);
+            db.Questions.Remove(enrollQuestion);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
