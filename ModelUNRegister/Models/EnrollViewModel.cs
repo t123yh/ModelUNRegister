747746755080d74d90c715b1b198a5479c8ff443
t123yh <t123yh@outlook.com>
2016-05-01 20:22:04 +0800

    修改 EnrollViewModel，添加用于创建的函数。

--- ModelUNRegister/Models/EnrollViewModel.cs
+++ ModelUNRegister/Models/EnrollViewModel.cs
@@ -8,6 +8,23 @@ namespace ModelUNRegister.Models
 {
     public class EnrollViewModel
     {
+        public static EnrollViewModel CreateFromUser(ApplicationUser user)
+        {
+            EnrollViewModel model = new EnrollViewModel()
+            {
+                Name = user.UserName,
+                School = user.EnrollRequest.School,
+                SubmissionTime = user.EnrollRequest.SubmissionTime,
+                Email = user.Email,
+                EmailVerificationTime = user.EnrollRequest.EmailVerificationTime,
+                PhoneNumber = user.PhoneNumber,
+                QQNumber = user.EnrollRequest.QQNumber,
+                Gender = user.EnrollRequest.Gender,
+                Grade = user.EnrollRequest.Grade
+            };
+            return model;
+        }
+
         [Required(ErrorMessage = "请填写你的姓名。")]
         [Display(Name = "姓名")]
         public string Name { get; set; }
@@ -16,16 +33,16 @@ namespace ModelUNRegister.Models
         [Display(Name = "所在学校")]
         public string School { get; set; }
 
-        //[Display(Name = "提交时间")]
-        //public DateTime SubmissionTime { get; set; }
+        [Display(Name = "提交时间")]
+        public DateTime SubmissionTime { get; set; }
 
         [Required(ErrorMessage = "请填写你的电子邮件地址。")]
         [EmailAddress(ErrorMessage = "电子邮件地址格式不正确。")]
         [Display(Name = "电子邮件地址")]
         public string Email { get; set; }
 
-        //[Display(Name = "Email 验证时间")]
-        //public DateTime? EmailVerificationTime { get; set; }
+        [Display(Name = "Email 验证时间")]
+        public DateTime? EmailVerificationTime { get; set; }
 
         [Required(ErrorMessage = "请填写你的手机号码。")]
         [RegularExpression(@"1[3|5|7|8|][0-9]{9}", ErrorMessage = "请填写正确的手机号码。")]
