54a07eb2c0f967cd606a9fc544bc903139b53bcc
t123yh <t123yh@outlook.com>
2016-05-05 22:50:00 +0800

    添加用于显示问题的界面。

---
+++ ModelUNRegister/Controllers/AnswerController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using ModelUNRegister.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelUNRegister.Controllers
+{
+    [Authorize]
+    public class AnswerController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: Answer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Answers()
+        {
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+
+            var answers = db.Questions.ToArray().Select(question =>
+            {
+                EnrollQuestionAnswer answer = db.Answers.Where(ans => ans.User.Id == user.Id && ans.Question.Id == question.Id).FirstOrDefault();
+                return new QuestionAnswerViewModel()
+                {
+                    AnswerContent = answer?.AnswerContent,
+                    AnswerId = answer?.Id,
+                    Question = question,
+                };
+            });
+
+            AnswersViewModel model = new AnswersViewModel() { Answers = answers.ToList() };
+
+            return View(model);
+        }
+
+    }
+}
---
+++ ModelUNRegister/Models/AnswerViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelUNRegister.Models
+{
+    public class QuestionAnswerViewModel
+    {
+        [Required]
+        [Display(Name = "回答内容")]
+        public string AnswerContent { get; set; }
+
+        public Guid? AnswerId { get; set; }
+
+        public EnrollQuestion Question { get; set; }
+    }
+
+    public class AnswersViewModel
+    {
+        public List<QuestionAnswerViewModel> Answers { get; set; }
+    }
+}
