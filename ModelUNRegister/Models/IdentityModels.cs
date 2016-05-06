c64867a8fe07b182d5113cf1891bb864695be48e
t123yh <t123yh@outlook.com>
2016-05-06 22:39:42 +0800

    添加从 Identity 中读取真实姓名的方法。

--- ModelUNRegister/Models/IdentityModels.cs
+++ ModelUNRegister/Models/IdentityModels.cs
@@ -16,6 +16,7 @@ namespace ModelUNRegister.Models
             // 请注意，authenticationType 必须与 CookieAuthenticationOptions.AuthenticationType 中定义的相应项匹配
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // 在此处添加自定义用户声明
+            userIdentity.AddClaim(new Claim("ActualName", this.ActualName));
             return userIdentity;
         }
 
---
+++ ModelUNRegister/Utilities/IdentityExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace ModelUNRegister.Utilities
+{
+    public static class IdentityExtensions
+    {
+        public static string GetActualName(this IIdentity identity)
+        {
+            var claim = ((ClaimsIdentity)identity).FindFirst("ActualName");
+            return (claim != null) ? claim.Value : string.Empty;
+        }
+    }
+}
