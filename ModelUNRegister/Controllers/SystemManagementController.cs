cc9d08a3c2e282da86150a4501af2d5c46aa1d01
t123yh <t123yh@outlook.com>
2016-05-07 20:25:19 +0800

    在系统管理界面添加报名人数指示。

--- ModelUNRegister/Controllers/SystemManagementController.cs
+++ ModelUNRegister/Controllers/SystemManagementController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ModelUNRegister.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,10 +12,22 @@ namespace ModelUNRegister.Controllers
     [Authorize(Roles = "Administrators")]
     public class SystemManagementController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: SystemManagement
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            ViewBag.RequestCount = await db.EnrollRequests.CountAsync();
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
