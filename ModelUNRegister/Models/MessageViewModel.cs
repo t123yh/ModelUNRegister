0e94bf73747e8fa958b8c0cd33bb2941cf241125
t123yh <t123yh@outlook.com>
2016-05-07 09:18:46 +0800

    添加用于显示消息的视图。

---
+++ ModelUNRegister/Models/MessageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using ModelUNRegister.Utilities;
+using System.Reflection;
+
+namespace ModelUNRegister.Models
+{
+    public enum BootstrapTheme
+    {
+        [Display(Name = "default")]
+        Default,
+        [Display(Name = "primary")]
+        Primary,
+        [Display(Name = "success")]
+        Success,
+        [Display(Name = "info")]
+        Inforamtion,
+        [Display(Name = "warning")]
+        Warning,
+        [Display(Name = "danger")]
+        Danger
+    }
+
+    public class MessageViewModel
+    {
+
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+
+        public BootstrapTheme Theme { get; set; }
+
+        public string ThemeString
+        {
+            get
+            {
+                return Theme.GetDisplayName();
+            }
+        }
+    }
+}
---
+++ ModelUNRegister/Utilities/DisplayNameHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelUNRegister.Utilities
+{
+    public static class DisplayNameHelper
+    {
+        public static string GetDisplayName(this Enum value)
+        {
+            // From http://www.csharpdeveloping.net/Snippet/how_to_get_display_name_displayattribute_from_enum_value
+
+            var type = value.GetType();
+            if (!type.IsEnum) throw new ArgumentException(String.Format("Type '{0}' is not Enum", type));
+
+            var members = type.GetMember(value.ToString());
+            if (members.Length == 0) throw new ArgumentException(String.Format("Member '{0}' not found in type '{1}'", value, type.Name));
+
+            var member = members[0];
+            var attributes = member.GetCustomAttributes(typeof(DisplayAttribute), false);
+            if (attributes.Length == 0) throw new ArgumentException(String.Format("'{0}.{1}' doesn't have DisplayAttribute", type.Name, value));
+
+            var attribute = (DisplayAttribute)attributes[0];
+            return attribute.GetName();
+        }
+    }
+}
