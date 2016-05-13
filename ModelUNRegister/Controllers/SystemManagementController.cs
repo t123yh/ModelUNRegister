c30b5707f38a59dfc8672f7f8f67fef095115ab6
t123yh <t123yh@outlook.com>
2016-05-14 00:56:07 +0800

    添加报名用户管理。

--- ModelUNRegister/Controllers/SystemManagementController.cs
+++ ModelUNRegister/Controllers/SystemManagementController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace ModelUNRegister.Controllers
 {
@@ -19,6 +20,43 @@ namespace ModelUNRegister.Controllers
         {
             ViewBag.RequestCount = await db.EnrollRequests.CountAsync();
             return View();
+        }
+
+        public async Task<ActionResult> EnrollList(int? page, string query)
+        {
+            IQueryable<EnrollRequest> users = from s in db.EnrollRequests
+                        orderby s.SubmissionTime
+                        select s;
+
+            var questionCount = await db.Questions.CountAsync();
+
+            if (query != "*")
+            {
+                if(string.IsNullOrWhiteSpace(query))
+                {
+                    query = "EmailConfirmed";
+                }
+                foreach (var condition in query.Split('|'))
+                {
+                    switch (condition)
+                    {
+                        case "EmailConfirmed":
+                            users = users.Where(r => r.User.EmailConfirmed);
+                            break;
+                        case "QuestionAnswered":
+                            users = users.Where(r => r.User.Answers.Count() == questionCount);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            ViewBag.QuestionCount = questionCount;
+
+            int pageNum = page.HasValue ? page.Value : 1;
+
+            return View(await users.Select(s => new EnrollListItem() { Request = s, AnswerCount = s.User.Answers.Count() }).ToPagedListAsync(pageNum, 10));
         }
 
         protected override void Dispose(bool disposing)
---
+++ ModelUNRegister/Models/EnrollListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelUNRegister.Models
+{
+    public class EnrollListItem
+    {
+        public EnrollRequest Request { get; set; }
+
+        [Display(Name = "回答问题")]
+        public int AnswerCount { get; set; }
+    }
+}
