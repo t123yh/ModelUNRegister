37513ad47976ef2309f935ccf7fbf4ef0a8c4879
t123yh <t123yh@outlook.com>
2016-05-14 21:41:32 +0800

    移动报名列表控制器。

---
+++ ModelUNRegister/Controllers/EnrollListController.cs
@@ -0,0 +1,63 @@
+﻿using ModelUNRegister.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelUNRegister.Controllers
+{
+    public class EnrollListController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public async Task<ActionResult> Index(int? page, string query)
+        {
+            IQueryable<EnrollRequest> users = from s in db.EnrollRequests
+                                              orderby s.SubmissionTime
+                                              select s;
+
+            var questionCount = await db.Questions.CountAsync();
+
+            if (query != "*")
+            {
+                if (string.IsNullOrWhiteSpace(query))
+                {
+                    query = "EmailConfirmed";
+                }
+                foreach (var condition in query.Split('|'))
+                {
+                    switch (condition)
+                    {
+                        case "EmailConfirmed":
+                            users = users.Where(r => r.User.EmailConfirmed);
+                            break;
+                        case "QuestionAnswered":
+                            users = users.Where(r => r.User.Answers.Count() == questionCount);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            ViewBag.QuestionCount = questionCount;
+
+            int pageNum = page.HasValue ? page.Value : 1;
+
+            return View(await users.Select(s => new EnrollListItem() { Request = s, AnswerCount = s.User.Answers.Count() }).ToPagedListAsync(pageNum, 10));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- ModelUNRegister/Controllers/SystemManagementController.cs
+++ ModelUNRegister/Controllers/SystemManagementController.cs
@@ -22,43 +22,6 @@ namespace ModelUNRegister.Controllers
             return View();
         }
 
-        public async Task<ActionResult> EnrollList(int? page, string query)
-        {
-            IQueryable<EnrollRequest> users = from s in db.EnrollRequests
-                        orderby s.SubmissionTime
-                        select s;
-
-            var questionCount = await db.Questions.CountAsync();
-
-            if (query != "*")
-            {
-                if(string.IsNullOrWhiteSpace(query))
-                {
-                    query = "EmailConfirmed";
-                }
-                foreach (var condition in query.Split('|'))
-                {
-                    switch (condition)
-                    {
-                        case "EmailConfirmed":
-                            users = users.Where(r => r.User.EmailConfirmed);
-                            break;
-                        case "QuestionAnswered":
-                            users = users.Where(r => r.User.Answers.Count() == questionCount);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-
-            ViewBag.QuestionCount = questionCount;
-
-            int pageNum = page.HasValue ? page.Value : 1;
-
-            return View(await users.Select(s => new EnrollListItem() { Request = s, AnswerCount = s.User.Answers.Count() }).ToPagedListAsync(pageNum, 10));
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
