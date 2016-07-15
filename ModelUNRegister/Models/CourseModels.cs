4b7ec714aa10cb6ff45479171a51b911a79661c5
t123yh <t123yh@outlook.com>
2016-07-15 22:49:58 +0800

    添加课程模型。

---
+++ ModelUNRegister/Models/CourseModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelUNRegister.Models
+{
+    public class Course
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Display(Name = "姓名")]
+        public string Name { get; set; }
+        [Display(Name = "来自")]
+        public string From { get; set; }
+        [Display(Name = "课程名称")]
+        public string CourseName { get; set; }
+        [Display(Name = "课程介绍")]
+        public string CourseContent { get; set; }
+    }
+}
