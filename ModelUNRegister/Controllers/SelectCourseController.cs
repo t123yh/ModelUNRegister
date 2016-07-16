1cbd8ab02761f3c11ce2a05562359ab8ed7848bd
t123yh <t123yh@outlook.com>
2016-07-16 15:30:53 +0800

    增加选课界面。

---
+++ ModelUNRegister/Controllers/SelectCourseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelUNRegister.Controllers
+{
+    public class SelectCourseController : Controller
+    {
+        // GET: SelectCourse
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
