29c5de99257ab6dad22ead3e5b4a816dc69eacbd
t123yh <t123yh@outlook.com>
2016-07-16 11:41:17 +0800

    修正课程模型。

--- ModelUNRegister/Models/CourseModels.cs
+++ ModelUNRegister/Models/CourseModels.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ModelUNRegister.Models
 {
@@ -10,13 +11,22 @@ namespace ModelUNRegister.Models
     {
         [Key]
         public Guid Id { get; set; }
+
+        [Required]
         [Display(Name = "姓名")]
         public string Name { get; set; }
+
         [Display(Name = "来自")]
+        [Required]
         public string From { get; set; }
+
         [Display(Name = "课程名称")]
+        [Required]
         public string CourseName { get; set; }
+
         [Display(Name = "课程介绍")]
+        [Required]
+        [AllowHtml]
         public string CourseContent { get; set; }
     }
 }
