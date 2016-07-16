54e60d3fb524eb7064eb9b38fef8e2868a7f4458
t123yh <t123yh@outlook.com>
2016-07-16 19:54:53 +0800

    增加显示课程。

--- ModelUNRegister/Controllers/SelectCourseController.cs
+++ ModelUNRegister/Controllers/SelectCourseController.cs
@@ -1,17 +1,49 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.Owin;
+using ModelUNRegister.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace ModelUNRegister.Controllers
 {
+    [Authorize]
     public class SelectCourseController : Controller
     {
-        // GET: SelectCourse
-        public ActionResult Index()
+        private ApplicationUserManager _userManager;
+
+        private ApplicationDbContext _db;
+
+        public ApplicationDbContext DbContext
         {
-            return View();
+            get
+            {
+                return _db ?? HttpContext.GetOwinContext().Get<ApplicationDbContext>();
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: SelectCourse
+        public async Task<ActionResult> Index()
+        {
+
+            var courses = await DbContext.Courses.ToArrayAsync();
+            var selected = (await UserManager.FindByIdAsync(User.Identity.Name)).Courses.Select(c => c.Id);
+            return View(new CourseSelectionViewModel() { Courses = courses, SelectedCourses = selected });
         }
     }
 }
---
+++ ModelUNRegister/Models/CourseSelectionModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelUNRegister.Models
+{
+    public class CourseSelectionViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Guid> SelectedCourses { get; set; }
+    }
+}
